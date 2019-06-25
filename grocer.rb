d0ed3159f4a929db6f793584b94f4808a851ab6e
Corbin Arnett <corbin.c.arnett@gmail.com>
2019-06-25 04:33:46 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,9 +1,41 @@
+
 def consolidate_cart(cart)
-  # code here
+  updated_cart = {}
+  cart.each do |item|
+
+    if updated_cart[item.keys[0]]
+      updated_cart[item.keys[0]][:count] += 1
+    else
+
+      updated_cart[item.keys[0]] = {
+            count: 1,
+            price: item.values[0][:price],
+            clearance: item.values[0][:clearance]
+      }
+    end
+  end
+updated_cart
 end
 
 def apply_coupons(cart, coupons)
-  # code here
+    coupons.each do |coupon|
+      if cart.keys.include? coupon[:item]
+        if cart[coupon[:item]][:count] >= coupon[:num]
+  new_name = "#{coupon[:item]} W/COUPON"
+  if cart[new_name]
+    cart[new_name][:count] += coupon[:num]
+  else
+    cart[new_name] = {
+      count: coupon[:num],
+      price: coupon[:cost]/coupon[:num],
+      clearance: cart[coupon[:item]][:clearance]
+    }
+  end
+  cart[coupon[:item]][:count] -= coupon[:num]
+      end
+    end
+  end
+cart
 end
 
 def apply_clearance(cart)
